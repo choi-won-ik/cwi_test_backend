cfdd6120fa2e9ef7bb294cb70a71f4eda2a79ff8
choi-won-ik <whwwhs7837@gmail.com>
2025-02-26 10:48:58 +0900

    [Fix] Board 상세 조회

--- src/main/java/com/example/cwi_test_backend/board/BoardController.java
+++ src/main/java/com/example/cwi_test_backend/board/BoardController.java
@@ -1,8 +1,10 @@
 package com.example.cwi_test_backend.board;
 
 import com.example.cwi_test_backend.board.model.Board;
+import com.example.cwi_test_backend.board.model.BoardDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class BoardController {
     @GetMapping("/list")
     public List<Board> list() {
         return boardService.list();
+    }
+
+    @GetMapping("/details/{boardIdx}")
+    public BoardDto.BoardDtailsResp details(@PathVariable Long boardIdx) {
+        return boardService.details(boardIdx);
     }
 }
